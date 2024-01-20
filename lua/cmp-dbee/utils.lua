365c92b1520fa1b16b43b1f51f4daa241fc9cdda
Mattias Sjödin <mattias.sjodin@northvolt.com>
2024-01-20 22:44:35 +0100

    feat: add utils

--- lua/cmp-dbee/utils.lua
+++ lua/cmp-dbee/utils.lua
@@ -1,6 +1,9 @@
 local M = {}
+
 -- Function to get the text of the line before the cursor
 function M:get_cursor_before_line()
+	-- local lines = vim.api.nvim_buf_get_lines(0, 0, vim.api.nvim_win_get_cursor(0)[1], false)
+
 	-- Get the current line number and cursor position
 	local line_number = vim.fn.line(".")
 	local col_number = vim.fn.col(".")
@@ -18,6 +21,24 @@ function M:get_cursor_before_line()
 
 	-- If the cursor is at the beginning of the first line, return an empty string
 	return ""
+end
+
+-- Function to get the schema from the line before the cursor
+function M:captured_schema(line)
+	local cursor_before_line = line or self:get_cursor_before_line()
+	-- take into account the keyword can be upper or lower case and stuff before it
+	local schema = cursor_before_line:match("[from|FROM|join|JOIN]%s+([^%.%s|%.]+)")
+	if schema then
+		return schema
+	end
+
+	return cursor_before_line:match('[from|FROM|join|JOIN]%s+"([^"]+)')
+end
+
+-- Function to get the table from the line before the cursor
+function M:capture_table_based_on_schema(line)
+	local cursor_before_line = line or self:get_cursor_before_line()
+	return cursor_before_line:match("[from|FROM|JOIN|join]%s+[^%.%s]+%.([^%.%s]+)")
 end
 
 return M
